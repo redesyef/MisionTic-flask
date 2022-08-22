72d7e1aac18843cc65854329671f1bea87409c96
redesyef <ymauriciop@gmail.com>
2022-08-22 09:52:04 -0500

    test response

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from waitress import serve
 app = Flask(__name__)
 cors = CORS(app)
 
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"]="Server running ..."
+    return jsonify(json)
+
 def loadFileConfig():
     with open('config.json') as f:
         data = json.load(f)
